c5ae81600239ac2543666881d4ee1f71ec82c40a
Asamaporn Pladsri <68784579+Jajahz@users.noreply.github.com>
2021-11-03 13:48:49 +0700

    Update Assignment6

--- Assignment 6/Tshirt.js
+++ Assignment 6/Tshirt.js
@@ -1,8 +1,8 @@
-export let tshirt = [
+export const tshirt = [
     {
         tshirtId: 'T001',
         tshirtName: 'RIPNDIP NERMRIDER BEACH TEE MINT WASH (RND6072)',
-        img: '',
+        img: './img/T001.jpg',
         tshirtDesc: '100% Cotton Cloud Wash Tee',
         tshirtPrice: 1690,
         tshirtStock: 12
@@ -10,23 +10,23 @@ export let tshirt = [
     {
         tshirtId: 'T002',
         tshirtName: 'PLEASURES CUT HERE TURTLENECK BLACK (P21F021)',
-        img: '',
+        img: './img/T002.jpg',
         tshirtDesc: '100% cotton, Standard fit, Turtleneck',
         tshirtPrice: 2990,
         tshirtStock: 9
     },
-    { 
-        tshirtId : 'T003',
+    {
+        tshirtId: 'T003',
         tshirtName: 'CARNIVAL X ONE PIECE THREE ADMIRALS T-SHIRT BLACK (DROP 1)',
-        img: '',
+        img: './img/T003.jpg',
         tshirtDesc: 'cotton 100%, graphic print, loose fit, crew neck',
         tshirtPrice: 1090,
-        tshirtStock: 119  
+        tshirtStock: 119
     },
     {
-        tshirtId : 'T004',
+        tshirtId: 'T004',
         tshirtName: 'CARNIVAL X ONE PIECE ZORO L/S T-SHIRT BLACK (DROP 1)',
-        img: '',
+        img: './img/T004.jpg',
         tshirtDesc: 'cotton 100%, graphic print, loose fit, crew neck, long sleeve',
         tshirtPrice: 1290,
         tshirtStock: 54
@@ -34,7 +34,7 @@ export let tshirt = [
     {
         tshirtId: 'T005',
         tshirtName: 'CARNIVAL X ONE PIECE ENEL HAWAII SHIRT MULTI (DROP 1)',
-        img: '',
+        img: './img/T005.jpg',
         tshirtDesc: 'rayon 100%, allover print, loose fit, camp collar',
         tshirtPrice: 2290,
         tshirtStock: 8
@@ -42,7 +42,7 @@ export let tshirt = [
     {
         tshirtId: 'T006',
         tshirtName: 'VAC CULTURE ONE PIECE GHOST PRINCESS LONG SLEEVE SHIRT (BLACK)',
-        img: '',
+        img: './img/T006.jpg',
         tshirtDesc: 'Drop 4 ของ V.A.C. Culture x One Piece',
         tshirtPrice: 890,
         tshirtStock: 13
@@ -50,7 +50,7 @@ export let tshirt = [
     {
         tshirtId: 'T007',
         tshirtName: 'CARNIVAL X HIROSHI NAGAI SS21 POOL HAWAII SHIRT MULTICOLOR-XXL',
-        img: '',
+        img: './img/T007.jpg',
         tshirtDesc: '100% rayon, Loose fit, Camp Collar, Allover print',
         tshirtPrice: 1861,
         tshirtStock: 21
@@ -58,9 +58,25 @@ export let tshirt = [
     {
         tshirtId: 'T008',
         tshirtName: 'VAC CULTURE POKEMON PIKACHU T-SHIRT WHITE-XL',
-        img: '',
+        img: './img/T008.jpg',
         tshirtDesc: 'V.A.C. Culture | Pokémon Collection',
         tshirtPrice: 890,
+        tshirtStock: 38
+    },
+    {
+        tshirtId: 'T009',
+        tshirtName: 'CHAMPION HOODED SWEATSHIRT BROWN (C3-T114-860)',
+        img: './img/T009.jpg',
+        tshirtDesc: 'CottonFrenchTerry350g / m2, Cotton100%',
+        tshirtPrice: 2303,
+        tshirtStock: 2
+    },
+    {
+        tshirtId: 'T010',
+        tshirtName: 'NIKE AS M NSW TEE M2Z PURPOSE (DJ1340-902)',
+        img: './img/T010.jpg',
+        tshirtDesc: ' 100% sustainable materials, using a blend of recycled polyester, recycled cotton and organic cotton fibers. The blend is at least 10% recycled fibers or at least 10% organic cotton fibers.',
+        tshirtPrice: 1600,
         tshirtStock: 38
     }
 ]
--- Assignment 6/productLink.js
+++ Assignment 6/productLink.js
@@ -1,29 +1,64 @@
-import { tshirt } from "./Tshirt";
+import {tshirt} from "./Tshirt.js";
 
-const divTshirtsEle = document.querySelector('#tshirt');
-console.log(divTshirtsEle);
+const divTshirtsEle = document.querySelector('#tshirtList');
+//querySelector ใช้เรียก css ที่ match กับ id tshirtList เพื่อให้ divTshirtsEle นั้นไปอยู่ตำแหน่งเดียวกับ id นั้น
 
+// loop ภายใน array tshirt[] ที่ import มา
+for (let tshirtele of tshirt) {
 const divTshirtEle = document.createElement('div');
-divTshirtEle.setAttribute('id', tshirt.tshirtId);
-console.log(divTshirtEle);
+divTshirtEle.setAttribute('id', tshirtele.tshirtId);
+divTshirtEle.setAttribute('class', "card-group");
+//สร้าง element div โดยให้ id เป็น tshirtId และ class เป็น card group จาก bootstrap
 
-const pTshirtId = document.createElement('p');
-pTshirtId.textContent = 'Id: ' + tshirt.tshirtId;
-divTshirtEle.appendChild(pTshirtId);
-console.log(divTshirtEle);
+const tshirtName = document.createElement('p');
+tshirtName.setAttribute('class', "h5");
+tshirtName.textContent = tshirtele.tshirtName;
+//สร้าง element tag p สำหรับ รายชื่อสินค้าจาก tshirtName ให้ class เป็น h5 (ขนาดตัวอักษรจาก bootstrap)
 
+const tshirtDesc = document.createElement('p');
+tshirtDesc.textContent = 'Description ' + tshirtele.tshirtDesc;
+//สร้าง element tag p สำหรับ คำบรรยายสินค้าจาก tshirtDesc 
 
-const ptshirtDesc = document.createElement('p');
-ptshirtDesc.textContent = 'Description ' + tshirt.tshirtDesc;
-divTshirtEle.appendChild(ptshirtDesc);
-console.log(divTshirtEle);
+const TshirtStock = document.createElement('p')
+TshirtStock.textContent = 'T-shirt Stock : ' + tshirtele.tshirtStock;
+//สร้าง element tag p สำหรับ เก็บจำนวนคงเหลือในคลัง หรือ tshirtStock
 
-const pTshirtStock = document.createElement('p')
-pTshirtStock.textContent = 'T-shirt Stock: ' + tshirt.tshirtStock;
-divTshirtEle.appendChild(pTshirtStock);
-console.log(divTshirtEle);
+const tshirtPrice = document.createElement('p');
+tshirtPrice.textContent = 'Price : ' + tshirtele.tshirtPrice + ' Baht.';
+//สร้าง element tag p สำหรับ ราคา หน่วยเป็นบาท หรือ tshirtPrice 
 
-const ptshirtPrice = document.createElement('p');
-ptshirtPrice.textContent = 'Price :' + tshirt.tshirtPrice + ' Baht.';
-divTshirtEle.appendChild(ptshirtPrice);
-console.log(divTshirtEle);
+const addtoCartButton = document.createElement('button');
+addtoCartButton.textContent = 'Add to cart';
+addtoCartButton.setAttribute("class","btn btn-outline-dark btn-lg");
+addtoCartButton.setAttribute("style","margin: 4px;");
+//สร้าง element button สำหรับปุ่มเพิ่มลงตะหร้า addtocart โดยใช้ bootsttrap ในการตกแต่งเพิ่มเติม 
+
+const tshirtImage = document.createElement('img');
+tshirtImage.setAttribute("src",tshirtele.img);
+tshirtImage.setAttribute("class","card-img-top" );
+tshirtImage.setAttribute("alt",tshirtele.tshirtId );
+//สร้าง element tag img สำหรับรูปภาพสินค้า โดย src จะเก็บที่อยู่ path ของรูปภาพใน folder, 
+//alt จะเก็บ tshirtid และ class เป็น card-img-top สำหรับ bootstrap
+
+const cardBody = document.createElement('div');
+cardBody.setAttribute("class","card-body");
+//สร้าง element div ที่มีclassเป็น card-body สำหรับส่วนที่เป็นเนื้อหา
+
+const card = document.createElement('div');
+card.setAttribute("class","card border-dark");
+//สร้าง element div ที่มี class เป็น card border-dark สำหรับส่วนที่เป็น card ของแต่ละสินค้า
+
+cardBody.appendChild(tshirtName);
+cardBody.appendChild(tshirtDesc);
+cardBody.appendChild(TshirtStock);
+cardBody.appendChild(tshirtPrice);
+//ในส่วนของ card body นำ element ประเภทข้อมูลมาใส่ เช่น ชื่อสินค้า จำนวนสินค้า ราคาสินค้า และ คำบรรยาย
+
+card.appendChild(tshirtImage)
+card.appendChild(cardBody);
+card.appendChild(addtoCartButton);
+//ในส่วนของ element ที่เป็น card จะนำ classbody มาใส่ (appendChild) รวมถึงรูปภาพและปุ่ม
+
+divTshirtEle.appendChild(card)
+divTshirtsEle.appendChild(divTshirtEle);
+}
