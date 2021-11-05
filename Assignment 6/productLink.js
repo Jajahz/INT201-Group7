52b4b0b97a1a6531e91cdc8babce11adf9b0bf0d
phxsrp <68784379+phxsrp@users.noreply.github.com>
2021-11-05 16:39:22 +0700

    Add files via upload

--- Assignment 6/productLink.js
+++ Assignment 6/productLink.js
@@ -1,64 +1,73 @@
-import {tshirt} from "./Tshirt.js";
+import { tshirt } from "./Tshirt.js";
 
-const divTshirtsEle = document.querySelector('#tshirtList');
+document.addEventListener("DOMContentLoaded", function() {
+    showTshirt(tshirt)
+  });
 //querySelector ใช้เรียก css ที่ match กับ id tshirtList เพื่อให้ divTshirtsEle นั้นไปอยู่ตำแหน่งเดียวกับ id นั้น
 
-// loop ภายใน array tshirt[] ที่ import มา
-for (let tshirtele of tshirt) {
-const divTshirtEle = document.createElement('div');
-divTshirtEle.setAttribute('id', tshirtele.tshirtId);
-divTshirtEle.setAttribute('class', "card-group");
-//สร้าง element div โดยให้ id เป็น tshirtId และ class เป็น card group จาก bootstrap
+const showTshirt = tshirt => {
 
-const tshirtName = document.createElement('p');
-tshirtName.setAttribute('class', "h5");
-tshirtName.textContent = tshirtele.tshirtName;
-//สร้าง element tag p สำหรับ รายชื่อสินค้าจาก tshirtName ให้ class เป็น h5 (ขนาดตัวอักษรจาก bootstrap)
+    const divTshirtsEle = document.querySelector('#tshirtList');
+    for (let tshirtele of tshirt) { //loop ภายใน array tshirt[] ที่ importมา
+        const divTshirtEle = document.createElement('div');
+        divTshirtEle.setAttribute('id', tshirtele.tshirtId);
+        divTshirtEle.setAttribute('class', "card-group");
+        //สร้าง element div โดยให้ id เป็น tshirtId และ class เป็น card group จาก bootstrap
 
-const tshirtDesc = document.createElement('p');
-tshirtDesc.textContent = 'Description ' + tshirtele.tshirtDesc;
-//สร้าง element tag p สำหรับ คำบรรยายสินค้าจาก tshirtDesc 
+        const tshirtName = document.createElement('p');
+        tshirtName.setAttribute('class', "h5");
+        tshirtName.textContent = tshirtele.tshirtName;
+        //สร้าง element tag p สำหรับ รายชื่อสินค้าจาก tshirtName ให้ class เป็น h5(ขนาดตัวอักษรจาก bootstrap)
 
-const TshirtStock = document.createElement('p')
-TshirtStock.textContent = 'T-shirt Stock : ' + tshirtele.tshirtStock;
-//สร้าง element tag p สำหรับ เก็บจำนวนคงเหลือในคลัง หรือ tshirtStock
+        const tshirtDesc = document.createElement('p');
+        tshirtDesc.textContent = 'Description ' + tshirtele.tshirtDesc;
+        //สร้าง element tag p สำหรับ คำบรรยายสินค้าจาก tshirtDesc 
 
-const tshirtPrice = document.createElement('p');
-tshirtPrice.textContent = 'Price : ' + tshirtele.tshirtPrice + ' Baht.';
-//สร้าง element tag p สำหรับ ราคา หน่วยเป็นบาท หรือ tshirtPrice 
+        const TshirtStock = document.createElement('p')
+        TshirtStock.textContent = 'T-shirt Stock : ' + tshirtele.tshirtStock;
+        //สร้าง element tag p สำหรับ เก็บจำนวนคงเหลือในคลัง หรือ tshirtStock 
 
-const addtoCartButton = document.createElement('button');
-addtoCartButton.textContent = 'Add to cart';
-addtoCartButton.setAttribute("class","btn btn-outline-dark btn-lg");
-addtoCartButton.setAttribute("style","margin: 4px;");
-//สร้าง element button สำหรับปุ่มเพิ่มลงตะหร้า addtocart โดยใช้ bootsttrap ในการตกแต่งเพิ่มเติม 
+        const tshirtPrice = document.createElement('p');
+        tshirtPrice.textContent = 'Price :' + tshirtele.tshirtPrice + ' Baht.';
+        //สร้าง element tag p สำหรับ ราคา หน่วยเป็นบาท หรือ tshirtPrice 
 
-const tshirtImage = document.createElement('img');
-tshirtImage.setAttribute("src",tshirtele.img);
-tshirtImage.setAttribute("class","card-img-top" );
-tshirtImage.setAttribute("alt",tshirtele.tshirtId );
-//สร้าง element tag img สำหรับรูปภาพสินค้า โดย src จะเก็บที่อยู่ path ของรูปภาพใน folder, 
-//alt จะเก็บ tshirtid และ class เป็น card-img-top สำหรับ bootstrap
+        const addtoCartButton = document.createElement('button');
+        addtoCartButton.textContent = 'Add to cart';
+        addtoCartButton.setAttribute("class", "btn btn-outline-dark btn-lg");
+        addtoCartButton.setAttribute("style", "margin: 4px;");
+        //สร้าง element button สำหรับปุ่มเพิ่มลงตะหร้า addtocart โดยใช้ bootsttrap ในการตกแต่งเพิ่มเติม 
 
-const cardBody = document.createElement('div');
-cardBody.setAttribute("class","card-body");
-//สร้าง element div ที่มีclassเป็น card-body สำหรับส่วนที่เป็นเนื้อหา
+        const tshirtImage = document.createElement('img');
+        tshirtImage.setAttribute("src", tshirtele.img);
+        tshirtImage.setAttribute("class", "card-img-top");
+        tshirtImage.setAttribute("alt", tshirtele.tshirtId);
+        //สร้าง element tag img สำหรับรูปภาพสินค้า โดย src จะเก็บที่อยู่ path ของรูปภาพใน folder, 
+        //alt จะเก็บ tshirtid และ class เป็น card-img-top สำหรับ bootstrap
 
-const card = document.createElement('div');
-card.setAttribute("class","card border-dark");
-//สร้าง element div ที่มี class เป็น card border-dark สำหรับส่วนที่เป็น card ของแต่ละสินค้า
+        const cardBody = document.createElement('div');
+        cardBody.setAttribute("class", "card-body");
+        //สร้าง element div ที่มีclassเป็น card-body สำหรับส่วนที่เป็นเนื้อหา
 
-cardBody.appendChild(tshirtName);
-cardBody.appendChild(tshirtDesc);
-cardBody.appendChild(TshirtStock);
-cardBody.appendChild(tshirtPrice);
-//ในส่วนของ card body นำ element ประเภทข้อมูลมาใส่ เช่น ชื่อสินค้า จำนวนสินค้า ราคาสินค้า และ คำบรรยาย
+        const card = document.createElement('div');
+        card.setAttribute("class", "card border-dark");
+        //สร้าง element div ที่มีclassเป็น card border-dark สำหรับส่วนที่เป็น card ของแต่ละสินค้า
 
-card.appendChild(tshirtImage)
-card.appendChild(cardBody);
-card.appendChild(addtoCartButton);
-//ในส่วนของ element ที่เป็น card จะนำ classbody มาใส่ (appendChild) รวมถึงรูปภาพและปุ่ม
+        cardBody.appendChild(tshirtName);
+        cardBody.appendChild(tshirtDesc);
+        cardBody.appendChild(TshirtStock);
+        cardBody.appendChild(tshirtPrice);
+        //ในส่วนของ card body นำ element ปรพเภทข้อมูลมาใส่ เช่น ชื่อสินค้า จำนวนสินค้า ราคาสินค้า และ คำบรรยาย
 
-divTshirtEle.appendChild(card)
-divTshirtsEle.appendChild(divTshirtEle);
+        card.appendChild(tshirtImage)
+        card.appendChild(cardBody);
+        card.appendChild(addtoCartButton);
+        //ในส่วนของ element ที่เป็น card จะนำ classbody มาใส่(append child) รวมถึงรูปภาพและปุ่ม
+
+        divTshirtEle.appendChild(card)
+        divTshirtsEle.appendChild(divTshirtEle);
+    }
 }
+
+const seacrchBar = document.getElementById();
+
+
