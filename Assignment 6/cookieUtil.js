654d4c1549e7524b623d83ad3e9b99c33181b239
phxsrp <68784379+phxsrp@users.noreply.github.com>
2021-11-15 23:16:43 +0700

    Add files via upload

--- Assignment 6/cookieUtil.js
+++ Assignment 6/cookieUtil.js
@@ -25,38 +25,38 @@ export class CookieUtil {
     
   static checkCookie() {
     const colbtn = document.getElementById('colbtn');
-    // เป็นการ check ว่าสุดท้ายเรากดปุ่มไปอยู่ที่ theme ใด โดย get id ของปุ่มมา
     let head = document.getElementById('navbar');
+    // let area = document.body;
       var theme = this.getCookie('theme');
       if (theme == 'default') {
       document.body.className = 'bg-dark';
       head.className = 'navbar navbar-light bg-light';
       colbtn.checked=false;
-      // เป็นการ fix ค่าว่าเรากดไปที่ default ตอนเรากดเข้ามาใหม่ เราจะเก็บค่า class เป็นแบบนี้
+        
       }
       if (theme == 'colorful') {
       document.body.className = 'bg-info';
       head.className = 'navbar navbar-light bg-danger';
       colbtn.checked=true;
       }
-      // เป็นการ fix ค่าว่าเรากดไปที่ default ตอนเรากดเข้ามาใหม่ เราจะเก็บค่า class เป็นแบบนี้
   }
   static unsunset(name){
-    CookieUtil.setCookie(name,"",new Date(0));
+    CookieUtil.setCookie(name,"",new Date(0))
   }
   }
 
 function guestName(name){ 
   let data = document.cookie;  //ดึงข้อมูลของ cookie เข้ามา
-  let arrayOfCookie = data.split('; ');  //แยกข้อมูล cookie ของแต่ละตัว จะตัดทุกครั้งที่เจอ ;
+  let arrayOfCookie = data.split('; '); //แยกข้อมูลcookie ของแต่ละตัว จะตัดทุกครั้งที่เจอ ;
   for(let i of arrayOfCookie){  //loop ของทั้งหมดที่มีใน cookie 
-      let key = i.split('=');  //แยกค่า key value ออกจากกัน
-      if(key[0] == name){  //เช็คค่า key ว่าเท่ากับ name ไหม
-          return key[1];  //ถ้าเท่ากัน return value
+      let key = i.split('='); //แยกค่า key value ออกจากกัน
+      if(key[0] == name){ //เช็คค่า key ว่าเท่ากับ name ไหม
+          return key[1]; //ถ้าเท่ากัน return value 
       }
   }
 }
 
 CookieUtil.setCookie("username", "asma");
-let user = document.querySelector("#user");
-user.textContent += `Login as : ${guestName("username")}`;
+let user = document.querySelector("#user")
+user.textContent += `Login as : ${guestName("username")}`
+
--- Assignment 6/event.js
+++ Assignment 6/event.js
@@ -1,3 +1,93 @@
+import { tshirts } from './Tshirt.js';
+import { cartEvents } from './cart.js';
+
+import  './productLink.js';
+
+export const showTshirt = (tshirts) => {
+  const divTshirtsEle = document.querySelector('#tshirtList');
+  divTshirtsEle.innerHTML = null;
+  // กำหนด inner html ของ tshirtlist (divTshirtsEle) ให้มีค่า = null 
+  // เพื่อไม่ให้มีข้อมูลสินค้าเก่าออกมาทุกครั้งที่มีการเรียก showTshirt()
+
+  if (tshirts.length == 0) {
+    const notFoundT = document.createElement("h1");
+    notFoundT.setAttribute('class', 'display-6 text-white');
+    notFoundT.setAttribute('style', 'text-align: center;margin-top: 80px; position:relative ');
+    notFoundT.textContent = 'Item Not Found';
+    divTshirtsEle.appendChild(notFoundT);
+
+  }
+
+  if (tshirts.length !== 0) {
+    divTshirtsEle.setAttribute("class", "row row-cols-1 row-cols-md-4 g-4")
+  }
+
+  for (let tshirtele of tshirts) {
+    //loop ภายใน array tshirt[] ที่ import มา
+    const divTshirtEle = document.createElement('div');
+    divTshirtEle.setAttribute('id', tshirtele.tshirtId);
+    divTshirtEle.setAttribute('class', 'card-group');
+    //สร้าง element div โดยให้ id เป็น tshirtId และ class เป็น card group จาก bootstrap
+
+    const tshirtName = document.createElement('p');
+    tshirtName.setAttribute('class', 'h5');
+    tshirtName.textContent = tshirtele.tshirtName;
+    //สร้าง element tag p สำหรับ รายชื่อสินค้าจาก tshirtName ให้ class เป็น h5(ขนาดตัวอักษรจาก bootstrap)
+
+    const tshirtDesc = document.createElement('p');
+    tshirtDesc.textContent = 'Description ' + tshirtele.tshirtDesc;
+    //สร้าง element tag p สำหรับ คำบรรยายสินค้าจาก tshirtDesc
+
+    const TshirtStock = document.createElement('p');
+    TshirtStock.textContent = 'T-shirt Stock : ' + tshirtele.tshirtStock;
+    //สร้าง element tag p สำหรับ เก็บจำนวนคงเหลือในคลัง หรือ tshirtStock
+
+    const tshirtPrice = document.createElement('p');
+    tshirtPrice.textContent = 'Price : ' + tshirtele.tshirtPrice + ' Baht.';
+    //สร้าง element tag p สำหรับ ราคา หน่วยเป็นบาท หรือ tshirtPrice
+
+    const addtoCartButton = document.createElement('button');
+    addtoCartButton.textContent = 'Add to cart';
+    addtoCartButton.setAttribute('class', 'btn btn-outline-dark btn-lg');
+    addtoCartButton.setAttribute('style', 'margin: 4px;');
+    addtoCartButton.addEventListener('click', () => {
+      cartEvents.cartNumber();
+      cartEvents.add(tshirtele);
+    });
+    //สร้าง element button สำหรับปุ่มเพิ่มลงตะหร้า addtocart โดยใช้ bootsttrap ในการตกแต่งเพิ่มเติม
+
+    const tshirtImage = document.createElement('img');
+    tshirtImage.setAttribute('src', tshirtele.img);
+    tshirtImage.setAttribute('class', 'card-img-top');
+    tshirtImage.setAttribute('alt', tshirtele.tshirtId);
+    //สร้าง element tag img สำหรับรูปภาพสินค้า โดย src จะเก็บที่อยู่ path ของรูปภาพใน folder,
+    //alt จะเก็บ tshirtid และ class เป็น card-img-top สำหรับ bootstrap
+
+    const cardBody = document.createElement('div');
+    cardBody.setAttribute('class', 'card-body');
+    //สร้าง element div ที่มี class เป็น card-body สำหรับส่วนที่เป็นเนื้อหา
+
+    const card = document.createElement('div');
+    card.setAttribute('class', 'card border-dark');
+    //สร้าง element div ที่มี class เป็น card border-dark สำหรับส่วนที่เป็น card ของแต่ละสินค้า
+
+    cardBody.appendChild(tshirtName);
+    cardBody.appendChild(tshirtDesc);
+    cardBody.appendChild(TshirtStock);
+    cardBody.appendChild(tshirtPrice);
+    //ในส่วนของ card body นำ element ประเภทข้อมูลมาใส่ เช่น ชื่อสินค้า จำนวนสินค้า ราคาสินค้า และ คำบรรยาย
+
+    card.appendChild(tshirtImage);
+    card.appendChild(cardBody);
+    card.appendChild(addtoCartButton);
+    //ในส่วนของ element ที่เป็น card จะนำ classbody มาใส่ (appendChild) รวมถึงรูปภาพและปุ่ม
+
+    // เป็นฟังก์ชันที่เอาไว้เรียก cartnumber เมื่อ Click          
+    divTshirtEle.appendChild(card);
+    divTshirtsEle.appendChild(divTshirtEle);
+  }
+};
+
 const searchbtn = document.getElementById('mySearch');
 // ตั้งให้ searchbtn คือ id mysearch
 searchbtn.addEventListener(
@@ -26,6 +116,7 @@ const searchBtn = document.getElementById("searchBtn");
 searchBtn.addEventListener(
   'click'
   , () => {
+    
     const keyType = searchInput.value.trim();
     const keyTypeLower = keyType.toLowerCase();
     const tshirtMatch = tshirts.filter(tshirt => {
@@ -41,6 +132,6 @@ searchBtn.addEventListener(
     }
     )
     console.log(tshirtMatch)
-    return showTshirt(tshirtMatch)
+  return showTshirt(tshirtMatch)
     // return method showTshirt เฉพาะที่เหมือนกับค่าที่ป้อนลงไป (keyTypeLower) หรือ สินค้าที่อยู่ใน tshirtMatch
   })
