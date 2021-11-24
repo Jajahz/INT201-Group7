af5d674e8893533061f817c107a5ac376332fd1e
phxsrp <siraphop.p@mail.kmutt.ac.th>
2021-11-24 15:45:22 +0700

    js update

--- Assignment 6/productLink.js
+++ Assignment 6/productLink.js
@@ -1,8 +1,5 @@
 import { tshirts } from './Tshirt.js';
-import { CookieUtil } from './cookieUtil.js'
-import { toggle } from './toggle.js';
 import { showTshirt,getPresentcart,getPresentSearchBar } from './event.js';
-
 
 document.addEventListener('DOMContentLoaded', function () {
   showTshirt(tshirts);
