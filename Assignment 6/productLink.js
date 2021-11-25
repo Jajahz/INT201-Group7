81e33826409d496e08b90a9a404af32e5290ef96
Asamaporn Pladsri <asamaporn.jajah@mail.kmutt.ac.th>
2021-11-25 20:53:51 +0700

    update

--- Assignment 6/productLink.js
+++ Assignment 6/productLink.js
@@ -1,12 +1,7 @@
 import { tshirts } from './Tshirt.js';
-<<<<<<< HEAD
-import { showTshirt,getPresentcart,getPresentSearchBar } from './event.js';
-=======
-import { showTshirt, getPresentcart, getPresentSearchBar } from './event.js';
->>>>>>> 24ae60a9af01baff7e6a37d8026e64f34db97b68
+import { productEvent } from './event.js';
 
 document.addEventListener('DOMContentLoaded', function () {
-  showTshirt(tshirts);
-  getPresentcart();
-  getPresentSearchBar();
+  productEvent.showTshirt(tshirts);
+  productEvent.updateAll();
 });
